470caa6136296f0739780dc44320bf3e486d7146
shashanksinha2002 <shashanksinha2002@gmail.com>
2021-08-13 20:20:03 +0530

    whatsapp clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTnvg4AyVWvnt4mWDA4121NWHomlBRxsU",
+  authDomain: "whats-app-clone-8797d.firebaseapp.com",
+  projectId: "whats-app-clone-8797d",
+  storageBucket: "whats-app-clone-8797d.appspot.com",
+  messagingSenderId: "837548994708",
+  appId: "1:837548994708:web:3436cebb5a2e20e4a4c481",
+  measurementId: "G-FJ10KVG7R8"
+};
